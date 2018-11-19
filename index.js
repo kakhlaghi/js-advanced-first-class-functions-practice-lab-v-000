902d0566e81c330c05868249a0b3bba7f5c968dc
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-19 17:31:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Code your solution in this file!
-const logDriverNames = function(someArray) {
+const logDriverNames = function (someArray) {
   return someArray.forEach(function(item){
     console.log(item.name)
     })
 }
 
-const logDriversByHometown= function(drivers, hometown) {
+const logDriversByHometown = function (drivers, hometown) {
   return drivers.forEach(fucntion(driver, hometown){
     if (driver.hometown == hometown){
       console.log(driver.name)
